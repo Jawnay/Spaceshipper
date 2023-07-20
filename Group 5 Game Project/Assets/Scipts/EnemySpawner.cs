7689046ec1e0680f2091cdc46c18935ffb95a7e2
Zach <91219995+zachd9757@users.noreply.github.com>
2023-07-20 11:18:00 -0700

    Added AI to Tutorial Level
    Baked NavMesh, added spawner and placed spawnpoint. Enemy spawns on level start. Enemy deals damage to player with attack.

--- Group 5 Game Project/Assets/Scipts/EnemySpawner.cs
+++ Group 5 Game Project/Assets/Scipts/EnemySpawner.cs
@@ -10,7 +10,7 @@ public class EnemySpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        SpawnBlueEnemies(2);
+        SpawnBlueEnemies(1);
     }
 
     // Update is called once per frame
