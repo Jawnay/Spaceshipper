2472349d36c1a3f049785debceaad73c581acb66
Zach <91219995+zachd9757@users.noreply.github.com>
2023-07-26 10:32:41 -0700

    Shopkeeper Dialog Cycle working
    Added dialogs and ability to click through them.

--- Group 5 Game Project/Assets/Scipts/ShopkeepBehavior.cs
+++ Group 5 Game Project/Assets/Scipts/ShopkeepBehavior.cs
@@ -9,10 +9,14 @@ public class ShopkeepBehavior : MonoBehaviour
 
     public GameObject dialogCanvas;
 
+    int currentDialog;
+    public GameObject[] dialogs;
+
     // Start is called before the first frame update
     void Start()
     {
         triggerInfo = dialogTrigger.GetComponent<ShopkeeperDialog>();
+        currentDialog = 0;
     }
 
     void Update()
@@ -23,6 +27,23 @@ public class ShopkeepBehavior : MonoBehaviour
             Vector3 newtarget = t.position;
             newtarget.y = transform.position.y;
             transform.LookAt(newtarget);
-        }        
+
+            dialogCanvas.SetActive(true);
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                dialogs[currentDialog].SetActive(false);
+                currentDialog++;
+                if (currentDialog == dialogs.Length)
+                {
+                    currentDialog = 0;
+                }
+                dialogs[currentDialog].SetActive(true);
+            }
+        }
+        else
+        {
+            dialogCanvas.SetActive(false);
+        } 
     }
 }
