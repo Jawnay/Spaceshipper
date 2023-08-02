31768c3a54e588a7b7e7011e1566571e2ec27bed
kailahulse <102646005+kailahulse@users.noreply.github.com>
2023-08-02 12:16:58 -0700

    Reload Level2

--- Group 5 Game Project/Assets/Scipts/GameOverScreen.cs
+++ Group 5 Game Project/Assets/Scipts/GameOverScreen.cs
@@ -14,4 +14,9 @@ public class GameOverScreen : MonoBehaviour
     {
         SceneManager.LoadScene(0);
     }
+
+    public void ChangeSceneToBuildIndexTwo()
+    {
+        SceneManager.LoadScene(2);
+    }
 }
